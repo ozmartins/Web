3835b286e38a5a3d9144b679f220688f178f19dc
Oseias da Silva Martins <oseias@ufsc.gov.br>
2025-12-06 19:13:35 -0300

    Adiciona nome amigável nas models.

--- sweet_princing_v2_recipe/forms.py
+++ sweet_princing_v2_recipe/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from  .models import Product, Supplier, Recipe, Ingredient
+from .models import Product, Supplier, Recipe, Ingredient, Purchase
 
 class ProductForm(forms.ModelForm):
     class Meta: 
@@ -7,6 +7,9 @@ class ProductForm(forms.ModelForm):
         fields = ["name"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "forma-control", "maxlength": 100})
+        }
+        labels = {
+            'name': 'Nome'
         }
 
 class IngredientForm(forms.ModelForm):
@@ -16,6 +19,9 @@ class IngredientForm(forms.ModelForm):
         widgets = {
             "name": forms.TextInput(attrs={"class": "forma-control", "maxlength": 100})
         }
+        labels = {
+            'name': 'Nome'
+        }
 
 class SupplierForm(forms.ModelForm):
     class Meta: 
@@ -23,6 +29,9 @@ class SupplierForm(forms.ModelForm):
         fields = ["name"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "forma-control", "maxlength": 100})
+        }
+        labels = {
+            'name': 'Nome'
         }
 
 class RecipeForm(forms.ModelForm):
@@ -48,4 +57,17 @@ class RecipeForm(forms.ModelForm):
             'product': 'Produto',
             'yields': 'Rendimento (porções)',
             'preparationTimeInMinutes': 'Tempo de preparo (em minutos)'
+        }
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ['supplier']
+        widgets = {
+            'supplier': forms.Select(attrs={
+                'class': 'form-select'
+            })
+        }
+        labels = {
+            'supplier': 'Fornecedor'
         }
--- sweet_princing_v2_recipe/models.py
+++ sweet_princing_v2_recipe/models.py
@@ -4,14 +4,22 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
+
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Fornecedor"
+        verbose_name_plural = "Fornecedores"
+
 
 class Recipe(models.Model):
     product = models.OneToOneField(
         'Product',
-        on_delete=models.RESTRICT,
-        related_name='recipe'
+        on_delete=models.RESTRICT    
     )
     yields = models.DecimalField(
         max_digits=5,
@@ -19,6 +27,28 @@ class Recipe(models.Model):
     )
     preparationTimeInMinutes = models.IntegerField()
 
+    class Meta:
+        verbose_name = "Receita"
+        verbose_name_plural = "Receitas"
+
+
+class Purchase(models.Model):
+    supplier = models.ForeignKey(
+        'Supplier',
+        on_delete=models.RESTRICT
+    )
+    create_at = models.DateField(auto_now_add=True, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Compra"
+        verbose_name_plural = "Compras"
+
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     lastCost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Ingrediente"
+        verbose_name_plural = "Ingredientes"
